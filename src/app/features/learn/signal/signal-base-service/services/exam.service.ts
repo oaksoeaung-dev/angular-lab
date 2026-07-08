b407f7653762a8a5bb82d60edf03f4adb7a5d9b6
oaksoeaung-dev <oaksoeaung.dev@gmail.com>
2026-07-08 23:14:12 +0630

    Update signal base service

--- src/app/features/learn/signal/signal-base-service/services/exam.service.ts
+++ src/app/features/learn/signal/signal-base-service/services/exam.service.ts
@@ -1,9 +1,14 @@
-import { Service, signal } from '@angular/core';
+import { computed, inject, Service, signal } from '@angular/core';
 import { Question } from '../models/question.model';
+import { Answer } from '../models/answer.model';
+import { BehaviorSubject, switchMap, tap } from 'rxjs';
+import { ExamGeneratorService } from './exam-generator.service';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Service()
 export class ExamService {
-  readonly questions = signal<Question[]>([
+  readonly #generateExam$ = new BehaviorSubject<number>(1);
+  readonly #questions = signal<Question[]>([
     {
       caption: 'How much is 4 + 4 ?',
       answers: ['4', '8', '12', '16'],
@@ -20,7 +25,58 @@ export class ExamService {
       correctAnswerIndex: 1,
     },
   ]);
+  readonly #userAnswers = signal<number[]>([]);
+  readonly #isBusy = signal<boolean>(false);
 
-  readonly userAnswers = signal<number[]>([]);
-  readonly isBusy = signal<boolean>(false);
+  public readonly userAnswers = computed(() =>
+    this.#userAnswers().map<Answer>((ans, index) => ({
+      userAnswerIndex: ans,
+      isCorrect: ans === this.questions()[index].correctAnswerIndex,
+    })),
+  );
+  public readonly questions = this.#questions.asReadonly();
+  public readonly isBusy = this.#isBusy.asReadonly();
+  public readonly currentQuestionIndex = computed(() => this.userAnswers().length);
+  public readonly currentQuestion = computed(() => this.questions()[this.currentQuestionIndex()]);
+  public readonly questionsCount = computed(() => this.questions().length);
+  public readonly isQuizDone = computed(() => this.userAnswers().length === this.questionsCount());
+  public readonly correctAnswers = computed(() =>
+    this.userAnswers().filter((ans) => ans.isCorrect),
+  );
+  public readonly correctAnswersCount = computed(() => this.correctAnswers().length);
+
+  public answerCurrentQuestion(answerIndex: number) {
+    this.#userAnswers.update((answers) => [...answers, answerIndex]);
+  }
+  readonly level = toSignal(this.#generateExam$);
+
+  increaseLevel() {
+    this.#generateExam$.next(this.#generateExam$.value + 1);
+  }
+
+  decreaseLevel() {
+    if (this.#generateExam$.value > 1) {
+      this.#generateExam$.next(this.#generateExam$.value - 1);
+    }
+  }
+
+  repeatLevel() {
+    this.#generateExam$.next(this.#generateExam$.value);
+  }
+
+  constructor() {
+    const generator = inject(ExamGeneratorService);
+
+    this.#generateExam$
+      .pipe(
+        tap((level) => this.#isBusy.set(true)),
+        switchMap((level) => generator.generateExam(level)),
+        tap((questions) => {
+          this.#questions.set(questions);
+          this.#userAnswers.set([]);
+          this.#isBusy.set(false);
+        }),
+      )
+      .subscribe();
+  }
 }
--- src/app/features/learn/signal/signal-base-service/signal-base-service.ts
+++ src/app/features/learn/signal/signal-base-service/signal-base-service.ts
@@ -1,18 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component, ElementRef, inject, signal, viewChild } from '@angular/core';
 import { Progress } from './components/progress/progress';
 import { Question } from './models/question.model';
 import { QuestionPresenter } from './components/question-presenter/question-presenter';
 import { Done } from './components/done/done';
+import { ExamService } from './services/exam.service';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-signal-base-service',
-  imports: [Progress, QuestionPresenter, Done],
+  imports: [Progress, QuestionPresenter, Done, MatButton],
   templateUrl: './signal-base-service.html',
 })
 export class SignalBaseService {
-  readonly question = signal<Question>({
-    caption: 'How much is 4 + 4?',
-    answers: ['4', '6', '8', '12'],
-    correctAnswerIndex: 2,
-  });
+  readonly store = inject(ExamService);
 }
