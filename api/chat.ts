69746cd726243cb425d7f7a734f471b9ceed2113
oaksoeaung-dev <oaksoeaung.dev@gmail.com>
2026-07-25 10:14:33 +0630

    Add instructions

--- api/chat.ts
+++ api/chat.ts
@@ -78,6 +78,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
           vector_store_ids: [VECTOR_STORE_ID],
         },
       ],
+      instructions: instructions
     });
 
     return res.status(200).json(response);
