0c515404920eafc4b4ce9f56e6601754e5c12bc9
oaksoeaung-dev <oaksoeaung.dev@gmail.com>
2026-07-25 10:37:40 +0630

    Add table style and list style

--- src/app/features/test/test.ts
+++ src/app/features/test/test.ts
@@ -39,30 +39,19 @@ export class Test {
   send() {
     this.isLoading.set(true);
     this.messages.update((prev) => [...prev, { from: 'user', message: this.userMessage() }]);
-    this.http
-      .post('/api/chat', { message: this.userMessage() })
-      .subscribe({
-        next: (x: any) => {
-          x.output
-            .filter((f: any) => f.type === 'message')
-            .forEach((c: any) => {
-              c.content
-                .filter((t: any) => t.type === 'output_text')
-                .forEach((t: any) => {
-                  this.messages.update((prev) => [...prev, { from: 'ai', message: t.text }]);
-                });
-            });
-
-          this.userMessage.set('');
-          this.isLoading.set(false);
-        },
-        error: () => {
-          this.messages.update((prev) => [
-            ...prev,
-            { from: 'ai', message: 'Sorry — something went wrong. Please try again.' },
-          ]);
-          this.isLoading.set(false);
-        },
-      });
+    this.http.post('/api/chat', { message: this.userMessage() }).subscribe({
+      next: (x: any) => {
+        this.messages.update((prev) => [...prev, { from: 'ai', message: x.output_text }]);
+        this.userMessage.set('');
+        this.isLoading.set(false);
+      },
+      error: () => {
+        this.messages.update((prev) => [
+          ...prev,
+          { from: 'ai', message: 'Sorry — something went wrong. Please try again.' },
+        ]);
+        this.isLoading.set(false);
+      },
+    });
   }
 }
